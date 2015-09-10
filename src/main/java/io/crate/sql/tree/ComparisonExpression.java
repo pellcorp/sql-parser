cd8970bcc586cf75fd34deb16a0d5cdfd9f0bb81
Jason Pell <jason.pell@veda.com.au>
2015-09-10 15:27:55 +1000

    initial import of sql-parser project from crate.io after conversion to maven

---
+++ src/main/java/io/crate/sql/tree/ComparisonExpression.java
@@ -0,0 +1,159 @@
+/*
+ * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.sql.tree;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableMap;
+
+public class ComparisonExpression
+        extends Expression
+{
+    public enum Type
+    {
+        EQUAL("="),
+        NOT_EQUAL("<>"),
+        LESS_THAN("<"),
+        LESS_THAN_OR_EQUAL("<="),
+        GREATER_THAN(">"),
+        GREATER_THAN_OR_EQUAL(">="),
+        IS_DISTINCT_FROM("IS DISTINCT FROM"),
+        REGEX_MATCH("~"),
+        REGEX_NO_MATCH("!~"),
+        REGEX_MATCH_CI("~*"),
+        REGEX_NO_MATCH_CI("!~*");
+
+        public static ImmutableMap<Type, Type> INVERSE_MAP = ImmutableMap.<Type, Type>builder()
+                .put(EQUAL, NOT_EQUAL)
+                .put(NOT_EQUAL, EQUAL)
+                .put(LESS_THAN, GREATER_THAN_OR_EQUAL)
+                .put(LESS_THAN_OR_EQUAL, GREATER_THAN)
+                .put(GREATER_THAN, LESS_THAN_OR_EQUAL)
+                .put(GREATER_THAN_OR_EQUAL, LESS_THAN)
+                .put(REGEX_MATCH, REGEX_NO_MATCH)
+                .put(REGEX_NO_MATCH, REGEX_MATCH)
+                .put(REGEX_MATCH_CI, REGEX_NO_MATCH_CI)
+                .put(REGEX_NO_MATCH_CI, REGEX_MATCH_CI)
+                .build();
+
+        private final String value;
+
+        Type(String value)
+        {
+            this.value = value;
+        }
+
+        public String getValue()
+        {
+            return value;
+        }
+
+        public Type inverse() {
+            return INVERSE_MAP.get(this);
+        }
+    }
+
+    private final Type type;
+    private final Expression left;
+    private final Expression right;
+
+    public ComparisonExpression(Type type, Expression left, Expression right)
+    {
+        Preconditions.checkNotNull(type, "type is null");
+        Preconditions.checkNotNull(left, "left is null");
+        Preconditions.checkNotNull(right, "right is null");
+
+        this.type = type;
+        this.left = left;
+        this.right = right;
+    }
+
+    public Type getType()
+    {
+        return type;
+    }
+
+    public Expression getLeft()
+    {
+        return left;
+    }
+
+    public Expression getRight()
+    {
+        return right;
+    }
+
+    @Override
+    public <R, C> R accept(AstVisitor<R, C> visitor, C context)
+    {
+        return visitor.visitComparisonExpression(this, context);
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ComparisonExpression that = (ComparisonExpression) o;
+
+        if (!left.equals(that.left)) {
+            return false;
+        }
+        if (!right.equals(that.right)) {
+            return false;
+        }
+        if (type != that.type) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = type.hashCode();
+        result = 31 * result + left.hashCode();
+        result = 31 * result + right.hashCode();
+        return result;
+    }
+
+    public static Predicate<ComparisonExpression> matchesPattern(final Type type, final Class<?> left, final Class<?> right)
+    {
+        return new Predicate<ComparisonExpression>()
+        {
+            @Override
+            public boolean apply(ComparisonExpression expression)
+            {
+                return expression.getType() == type &&
+                        left.isAssignableFrom(expression.getLeft().getClass()) &&
+                        right.isAssignableFrom(expression.getRight().getClass());
+            }
+        };
+    }
+}
+
---
+++ src/main/java/io/crate/sql/tree/ShowColumns.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.sql.tree;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class ShowColumns
+        extends Statement
+{
+    private final QualifiedName table;
+
+    public ShowColumns(QualifiedName table)
+    {
+        this.table = checkNotNull(table, "table is null");
+    }
+
+    public QualifiedName getTable()
+    {
+        return table;
+    }
+
+    @Override
+    public <R, C> R accept(AstVisitor<R, C> visitor, C context)
+    {
+        return visitor.visitShowColumns(this, context);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hashCode(table);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) {
+            return true;
+        }
+        if ((obj == null) || (getClass() != obj.getClass())) {
+            return false;
+        }
+        ShowColumns o = (ShowColumns) obj;
+        return Objects.equal(table, o.table);
+    }
+
+    @Override
+    public String toString()
+    {
+        return MoreObjects.toStringHelper(this)
+                .add("table", table)
+                .toString();
+    }
+}
